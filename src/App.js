3fcec09035d1e873085bc37ff2df12835e7f4734
Tim Finnigan <tim.finnigan1@gmail.com>
2020-10-05 16:45:33 -0400

    init setup

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
 import React from 'react';
-import { GoogleMap } from 'react-google-maps';
+import { GoogleMap, withGoogleMap, withScriptjs } from 'react-google-maps';
 
 function Map() {
     return (
         <GoogleMap
             defaultZoom={5}
-            defaultCenter={{ lat: 37.0902, long: 95.7129 }}
+            defaultCenter={{ lat: 37.0902, lng: -95.7129 }}
         />
     );
 }
 
+const WrappedMap = withScriptjs(withGoogleMap(Map));
+
 const App = () => {
-    return <div>yoo</div>;
+    return (
+        <div style={{ width: '100vw', height: '100vh' }}>
+            <WrappedMap
+                googleMapURL={`https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_GOOGLE_KEY}&v=3.exp&libraries=geometry,drawing,places`}
+                loadingElement={<div style={{ height: '100%' }} />}
+                containerElement={<div style={{ height: '100%' }} />}
+                mapElement={<div style={{ height: '100%' }} />}
+            />
+        </div>
+    );
 };
 
 export default App;
