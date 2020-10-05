c8b43866a1d767e89e251815190aa16992305468
Tim Finnigan <tim.finnigan1@gmail.com>
2020-10-05 16:25:00 -0400

    install react-google-maps

--- src/App.js
+++ src/App.js
@@ -1,4 +1,14 @@
 import React from 'react';
+import { GoogleMap } from 'react-google-maps';
+
+function Map() {
+    return (
+        <GoogleMap
+            defaultZoom={5}
+            defaultCenter={{ lat: 37.0902, long: 95.7129 }}
+        />
+    );
+}
 
 const App = () => {
     return <div>yoo</div>;
